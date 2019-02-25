74d28c1722dc7dd2fdcbfd0ac8e3650855f324e4
PoweringBCR <richard.hainsworth@syd.catholic.edu.au>
2019-02-25 14:42:34 +1100

    Update chart.js

--- energy/chart.js
+++ energy/chart.js
@@ -39,7 +39,7 @@ function renderMainChart() {
     // stacked: false,
     title: {
       display: true,
-      text: <font colour="#B9764A">"Consumption vs <font colour ="#0CD2D7">Production"
+      text: <font colour="#B9764A">"Consumption </font> vs <font colour ="#0CD2D7">Production"</font>
       
     },
     legend: {
